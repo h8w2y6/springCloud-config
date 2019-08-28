f8400c898beff0d331dcdf3c95393c52cc69c8dd
huangwenyong <huangwenyong@szzt.com.cn>
2019-08-28 17:46:26 +0800

    first commit

---
+++ config-client/src/main/java/cn/xtft/springcloud/configclient/VsersionController.java
@@ -0,0 +1,18 @@
+package cn.xtft.springcloud.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class VsersionController {
+    @Value("${version}")
+    private String version;
+
+    @RequestMapping("/version")
+    public String version(){
+        return version;
+    }
+}
